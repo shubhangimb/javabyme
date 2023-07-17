3e97e43d745ed63f88402f27b1bf3da20345b666
shubhangimb <bhaisare.shubha@gmail.com>
2023-07-17 12:25:32 +0530

    my frist git

---
+++ src/abhi/abhi.java
@@ -0,0 +1,10 @@
+package abhi;
+
+public class abhi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/javagit/gitbyme.java
@@ -0,0 +1,13 @@
+package javagit;
+
+public class gitbyme {
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+     System.out.println("hi this me to do");
+
+     System.out.println("try to karna padega");
+     System.out.println("try on Youtube");
+     
+	}
+}
