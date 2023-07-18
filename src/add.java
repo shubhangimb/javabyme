d4b523f91826731f50df18d109d9f45ee1ec78eb
shubhangimb <139380804+shubhangimb@users.noreply.github.com>
2023-07-18 12:01:30 +0530

    Add files via upload

---
+++ src/add.java
@@ -0,0 +1,12 @@
+package twonumadd;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=23;
+		int b=20;
+		System.out.println(a+b);	
+	}
+
+}
